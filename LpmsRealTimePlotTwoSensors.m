bafc64f5d0da5e33d7f63f8be5772b70b6ad673b
AldoContreGo <aldocontrego@gmail.com>
2017-12-14 17:07:55 +0100

    Multiple Sensors
    Code to see data from multiple sensors

---
+++ LpmsRealTimePlotTwoSensors.m
@@ -0,0 +1,66 @@
+close all
+clear
+clc
+%% Initial Parameters
+samples = 400;          % number of samples to record (seconds / 100)
+nCount = 1;             % starting number
+fprintf('Script to real time plot LPMS sensor data with %d data width \n', samples);
+
+%% Code to Serial port selection (TODO)
+COMPortS1 = '/dev/tty.SLAB_USBtoUART';
+COMPortS2 = '/dev/tty.SLAB_USBtoUART9';
+%% Comunication parameters
+baudrate = 921600;          % rate at which information is transferred
+lpSensor1 = lpms();         % function lpms sensor given by LPMS
+lpSensor2 = lpms();         % function lpms sensor given by LPMS
+
+accDataS1 = zeros(samples,3);
+accDataS2 = zeros(samples,3);
+%% Connect to sensor 1
+if ( ~lpSensor1.connect(COMPortS1, baudrate) )
+    disp('sensor 1 not connected')
+    return 
+end
+disp('sensor 1 connected')
+%% Connect to sensor 2
+if ( ~lpSensor2.connect(COMPortS2, baudrate) )
+    disp('sensor 2 not connected')
+    return 
+end
+disp('sensor 2 connected')
+%% Set streaming mode
+lpSensor1.setStreamingMode();
+lpSensor2.setStreamingMode();
+%% Loop Plot
+figure('doublebuffer','on', ...
+       'CurrentCharacter','a', ...
+       'WindowStyle','modal')
+   
+while double(get(gcf,'CurrentCharacter'))~=27
+    nDataS1 = lpSensor1.hasSensorData();
+    nDataS2 = lpSensor2.hasSensorData();
+    for i=1:nDataS1
+        dS1 = lpSensor1.getQueueSensorData();
+        dS2 = lpSensor2.getQueueSensorData();
+        if nCount == samples
+            accDataS1=accDataS1(2:end, :);
+            accDataS2=accDataS2(2:end, :);
+        else
+            nCount = nCount + 1;
+        end
+        accDataS1(nCount,:) = dS1.acc;
+        accDataS2(nCount,:) = dS2.acc;
+    end
+    plot(1:samples,accDataS1)
+    grid on;
+    title(sprintf('ts = %fs', dS1.timestamp))
+    drawnow
+end
+
+set(gcf,'WindowStyle','normal');
+if (lpSensor1.disconnect())
+    disp('sensor 1 disconnected')
+end
+if (lpSensor2.disconnect())
+    disp('sensor 2 disconnected')
+end
--- SerialPorts.m
+++ SerialPorts.m
@@ -1,8 +1,8 @@
 clear all
 clc
 fprintf('%s \n',seriallist);
-prompt = 'Which port? [1-16] Zero to Exit: ';
-x = input(prompt);
+qtyPorts = length(seriallist);
+x = input(['Which port of the list? [1-' num2str(qtyPorts) ']. Zero to Exit: ']);
 if x == 0
     return
 end
@@ -14,3 +14,8 @@ for n = seriallist
     sCount = sCount + 1;
 end
 disp(COMPort)
+
+% /dev/cu.SLAB_USBtoUART 
+% /dev/tty.SLAB_USBtoUART 
+% /dev/cu.SLAB_USBtoUART3 
+% /dev/tty.SLAB_USBtoUART3 
