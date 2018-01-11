e53b1a6c4e0b83250e1392defcd4684b4d7c471b
Aldo Contreras <aldocontrego@gmail.com>
2018-01-11 16:15:43 +0100

    comments added
    comments

--- SerialPorts.m
+++ SerialPorts.m
@@ -1,7 +1,9 @@
+% This code shows the serial ports connected to the computer
+% You choose the port you want to connect
 clear all
 clc
 fprintf('%s \n',seriallist);
-prompt = 'Which port? [1-16] Zero to Exit: ';
+prompt = 'Select port? [1-16] Zero to Exit: ';
 x = input(prompt);
 if x == 0
     return
