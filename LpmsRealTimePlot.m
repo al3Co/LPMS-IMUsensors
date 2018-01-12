1317a19f09e9d96279e48398be9a4c20ab74890c
Aldo Contreras <aldocontrego@gmail.com>
2018-01-12 18:00:32 +0100

    comments and improvements
    comments and improvements added

--- LpmsRealTimePlot.m
+++ LpmsRealTimePlot.m
@@ -2,47 +2,50 @@ close all
 clear
 clc
 
-% Parameters
+%% Parameters
 T = 400;        % number of samples to record (seconds / 100)
 nCount = 1;     % starting number
+accData = zeros(T,3);
 fprintf('Script to real time plot LPMS sensor data with %d data width \n', T);
 
-% code to Serial port selection
-fprintf('%s \n',seriallist);
-prompt = 'Which port? [1-16] Zero to Exit: ';
+%% code to Serial port selection
+serials = seriallist;
+len = length(seriallist);
+for serialPort = 1:len
+    fprintf('%d - %s \n', serialPort, serials(serialPort));
+end
+prompt = (['Select port number. [' num2str(len) ' available(s)]. Zero to Exit: ']);
 x = input(prompt);
 if x == 0
-    disp('LPMS Sensors usb virtual com port(VCP) functionality is disabled by default. Please use VCPConversionTool to enable VCP support.');
+    disp('Aborted')
     return
 end
-sCount = 1;
-for n = seriallist
-    if sCount == x
-        COMPort = n;
+if x > len
+    disp('Invalid number')
+    return
+end
+for n = 1:len
+    if n == x
+        COMPort = serials(n);
     end
-    sCount = sCount + 1;
 end
 disp(COMPort)
 
-% Comunication parameters
+%% Comunication parameters
 baudrate = 921600;          % rate at which information is transferred
 lpSensor = lpms();          % function lpms sensor given by LPMS
 
-accData = zeros(T,3);
-
-
-
-% Connect to sensor
+%% Connect to sensor
 if ( ~lpSensor.connect(COMPort, baudrate) )
     disp('sensor not connected')
     return 
 end
 disp('sensor connected')
 
-% Set streaming mode
+%% Set streaming mode
 lpSensor.setStreamingMode();
 
-% Loop Plot
+%% Loop Plot
 figure('doublebuffer','on', ...
        'CurrentCharacter','a', ...
        'WindowStyle','modal')
@@ -60,11 +63,12 @@ while double(get(gcf,'CurrentCharacter'))~=27
     end
     plot(1:T,accData)
     grid on;
-    title(sprintf('ts = %fs', d.timestamp))
+    title(sprintf('ts = %5.2f', d.timestamp))
     drawnow
 end
-
 set(gcf,'WindowStyle','normal');
+
+%% disconnect
 if (lpSensor.disconnect())
     disp('sensor disconnected')
 end
