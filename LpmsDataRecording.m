bd1e79417a0705e863df840d6912edb2bb4ac5bb
Aldo Contreras <aldocontrego@gmail.com>
2018-01-12 18:00:03 +0100

    comments and improvements
    comments and improvements added

--- LpmsDataRecording.m
+++ LpmsDataRecording.m
@@ -2,47 +2,58 @@ close all
 clear
 clc
 
-% Parameters
+%% Parameters
 nData = 500;    % number of samples to record (seconds / 100)
 nCount = 1;     % starting number
 fprintf('Script to record LPMS sensor data with %d data \n', nData);
 
-% code to Serial port selection
-fprintf('%s \n',seriallist);
-prompt = 'Which port? [1-16] Zero to Exit: ';
+%% code to Serial port selection
+serials = seriallist;
+len = length(seriallist);
+for serialPort = 1:len
+    fprintf('%d - %s \n', serialPort, serials(serialPort));
+end
+prompt = (['Select port? [' num2str(len) ' available(s)]. Zero to Exit: ']);
 x = input(prompt);
+
 if x == 0
-    disp('LPMS Sensors usb virtual COM port(VCP) functionality is disabled by default. Please use VCPConversionTool to enable VCP support.');
+    disp('Aborted')
     return
 end
-sCount = 1;
-for n = seriallist
-    if sCount == x
-        COMPort = n;
+
+if x > len
+    disp('Invalid number')
+    return
+end
+
+for n = 1:len
+    if n == x
+        COMPort = serials(n);
     end
-    sCount = sCount + 1;
 end
 disp(COMPort)
 
-% Comunication parameters      
+%% Comunication parameters      
 baudrate = 921600;          % rate at which information is transferred
 lpSensor = lpms();          % function lpms API sensor given by LPMS
 
+%% storage variables
 ts = zeros(nData,1);
 accData = zeros(nData,3);
 quatData = zeros(nData,4);
 
 
-% Connect to sensor
+%% Connect to sensor
 if ( ~lpSensor.connect(COMPort, baudrate) )
     disp('sensor not connected')
     return 
 end
 disp('sensor connected')
 
-% Set streaming mode
+%% Set streaming mode
 lpSensor.setStreamingMode();
 
+%% reading sensor
 disp('Accumulating sensor data')
 while nCount <= nData
     d = lpSensor.getQueueSensorData();
