c8d9ef7dd6b6073ed51a7f699e7434a32ceae69e
Aldo Contreras <aldocontrego@gmail.com>
2018-01-19 12:36:34 +0100

    plot for two sensor
    plot for two sensor

--- readAndPlotPosition.m
+++ readAndPlotPosition.m
@@ -1,4 +1,4 @@
-%% This is a script that reads data from a single sensor and plot its orientation
+%% (18-01-2018) This is a script that reads data from a single sensor and plot its orientation
 close all
 clear
 clc
@@ -6,28 +6,39 @@ clc
 %% Parameters
 fprintf('Script to plot the orientation');
 
+%% Code to Serial port selection
+COMPort1 = 'COM3';
+COMPort2 = 'COM4';
+
 %% Comunication parameters
-COMPort = "COM3";
 baudrate = 921600;          % rate at which information is transferred
-lpSensor = lpms();          % function lpms API sensor given by LPMS
+lpSensor1 = lpms();         % function lpms API sensor 1 given by LPMS
+lpSensor2 = lpms();         % function lpms API sensor 2 given by LPMS
 
 %% Connect to sensor
-disp('Connecting to sensor ...')
-if ( ~lpSensor.connect(COMPort, baudrate) )
+disp('Connecting to sensors ...')
+if ( ~lpSensor1.connect(COMPort, baudrate) )
     disp('Sensor not connected')
     return 
 end
-disp('Sensor connected')
+if ( ~lpSensor2.connect(COMPort2, baudrate) )
+    disp('Sensor 2 not connected')
+    return
+end
+disp('Sensors connected')
 
 %% Setting streaming mode
-disp('Setting mode ...')
-lpSensor.setStreamingMode();
+disp('Setting mode')
+lpSensor1.setStreamingMode();
+lpSensor2.setStreamingMode();
 
+%% Physical data
 a1 = 1;         % length arm
 a2 = 1;         % length elbow 
 theta1 = 0;     % initial angle a1
 theta2 = 0;     % initial angle a2
 data = [a1, a2, theta1 , theta2];
+
 %% Loop Plot
 disp('Plotting ...')
 figure('doublebuffer','on', ...
@@ -35,20 +46,25 @@ figure('doublebuffer','on', ...
        'WindowStyle','modal')
 
 while double(get(gcf,'CurrentCharacter'))~=27
-    clf;
-    %d = lpSensor.getQueueSensorData();
-    d = lpSensor.getCurrentSensorData();
-    disp(d)
-    if (~isempty(d))
-        [yaw, pitch, roll] = quat2angle(d.quat);
-        data(4) = roll; % yaw or roll, depending the position of the sensor on the arm-frame
-        funcDrawSensor(data);
+    d1 = lpSensor1.getCurrentSensorData();
+    d2 = lpSensor2.getCurrentSensorData();
+    if (~isempty(d1))
+        [yawS1, pitchS1, rollS1] = quat2angle(d1.quat);
+        data(3) = rollS1; % yaw or roll, depending the position of the sensor on the arm-frame
     end
+    if (~isempty(d2))
+        [yawS2, pitchS2, rollS2] = quat2angle(d2.quat);
+        data(4) = rollS2; % yaw or roll, depending the position of the sensor on the arm-frame
+    end
+    funcDrawSensor(data);
 end
 set(gcf,'WindowStyle','normal');
 
 %% Disconnecting
 disp('Done')
-if (lpSensor.disconnect())
-    disp('Sensor disconnected')
+if (lpSensor1.disconnect())
+    disp('Sensor 1 disconnected')
+end
+if (lpSensor2.disconnect())
+    disp('Sensor 2 disconnected')
 end
