f02207a1c0c800d4d13f5b0e01ec261830ea6b16
Aldo Contreras <aldocontrego@gmail.com>
2018-01-22 19:17:11 +0100

    Improvements
    Improvements

--- funcCalcPosition.m
+++ funcCalcPosition.m
@@ -1,25 +1,32 @@
-% (19-01-2018) function to integrate acceleration to ver and pos
+% (19-01-2018) function to find position
 %the coloumn should contain the data in the following format:
-%col   data
-%1     actual X lineal Accel 
-%2     actual Y lineal Accel 
-%3     actual Z lineal Accel
-%col   time
-%1     timestamp
-%col   pastData
-%1     past X lineal Accel 
-%2     past Y lineal Accel 
-%3     past Z lineal Accel
+%col   theta_Acc
+%1     actual X acc 
+%2     actual Y acc
+%3     actual Z acc
+%col   theta_Mag
+%1     actual X Mag
+%2     actual Y Mag
+%3     actual Z Mag
 
-function [despX despY despZ] = funcCalcPosition(data, time, pastData)
+function [X, Y, Z] = funcCalcPosition(theta_Acc, theta_Mag)
+clf;
 
-disp(data)
+theta_i = theta_Acc(1);
+theta_r = asin(sin(theta_Acc(3))/cos(theta_Acc(1)));
+theta_h = theta_Mag(1)/cos(theta_Acc(1));
 
-vX = pastData(1) + data(1) * time;
-dX = 
+L1 = 1;
 
+X = (cos(theta_i)*cos(theta_h) - sin(theta_i)*sin(theta_r)*sin(theta_h)) * L1;
+Y = (sin(theta_i)*cos(theta_h) + cos(theta_i)*sin(theta_r)*sin(theta_h)) * L1;
+Z = -cos(theta_r)*sin(theta_h) * L1;
 
-despX = data(1);
-despY = data(2);
-despZ = data(3);
+grid on;
+hold on;
+
+plot3([0;X],[0;Y],[0;Z],'r')
+
+drawnow
+
 end
