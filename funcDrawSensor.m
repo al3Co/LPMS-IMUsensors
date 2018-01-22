92ef1d2f3f22cb057af6853c08d69f34cae8ee40
Aldo Contreras <aldocontrego@gmail.com>
2018-01-22 19:16:34 +0100

    improvements
    improvements

--- funcDrawSensor.m
+++ funcDrawSensor.m
@@ -3,26 +3,29 @@
 %col   data
 %1     length arm
 %2     length elbow 
-%3     theta a1 (angle theta1)
-%4     theta a2 (angle theta2)
+%3     theta L1 (angle elbow theta1)
+%4     theta L1 (angle elbow theta2)
+%5     theta L2 (angle wirst theta2)
 
 function funcDrawSensor(data)
 
 clf;
 
-a1 = data(1);
-a2 = data(2);
+L1 = data(1);
+L2 = data(2);
 theta1 = data(3);
-theta2 = data(4);
+theta2 = data(3);
+
+theta3 = data(5);
 
 
-A1 = [cos(theta1) -sin(theta1) 0 a1*cos(theta1);
-    sin(theta1) cos(theta1) 0 a1*sin(theta1);
+A1 = [cos(theta1) -sin(theta1) 0 L1*cos(theta1);
+    sin(theta1) cos(theta1) 0 L1*sin(theta1);
     0 0 1 0;
     0 0 0 1];
 
-A2 = [cos(theta2) -sin(theta2) 0 a2*cos(theta2);
-    sin(theta2) cos(theta2) 0 a2*sin(theta2);
+A2 = [cos(theta3) -sin(theta3) 0 L2*cos(theta3);
+    sin(theta3) cos(theta3) 0 L2*sin(theta3);
     0 0 1 0;
     0 0 0 1];
 
@@ -31,11 +34,11 @@ T20 = A1*A2;
 
 xS1 = (0 + T10(1,4))/2;
 yS1 = (0 + T10(2,4))/2;
-circ1Pos = [(xS1) (yS1) (0.1*a1) (0.1*a1)]; %[x y w h]
+circ1Pos = [(xS1) (yS1) (0.1*L1) (0.1*L1)]; %[x y w h]
 
 xS2 = (T10(1,4) + T20(1,4))/2;
 yS2 = (T10(2,4) + T20(2,4))/2;
-circ2Pos = [(xS2) (yS2) (0.1*a2) (0.1*a2)]; %[x y w h]
+circ2Pos = [(xS2) (yS2) (0.1*L2) (0.1*L2)]; %[x y w h]
 
 grid on;
 hold on;
@@ -45,7 +48,7 @@ plot([0;T10(1,4)],[0;T10(2,4)],'r')
 plot([T10(1,4);T20(1,4)],[T10(2,4);T20(2,4)],'g')
 % axis tight
 % axis equal
-axis([(-2*a1) (2*a1) (-2*a2) (2*a2)])
+axis([(-2*L1) (2*L1) (-2*L2) (2*L2)])
 
 % % for 3D plot:
 % % A1 vector
