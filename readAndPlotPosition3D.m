008edd3223e79545b41ce1cc7fc1e6d48ae23cc5
Aldo Contreras <aldocontrego@gmail.com>
2018-01-24 12:19:11 +0100

    comments added
    comments added

--- readAndPlotPosition3D.m
+++ readAndPlotPosition3D.m
@@ -1,4 +1,10 @@
 %% (18-01-2018) This script draw two sensors porition
+% Packages used:
+% LPMS API for Matlab
+% quat2angle from Aerospace Toolbox https://es.mathworks.com/help/aerotbx/ug/quat2angle.html
+% funcPlot3DPosV4 for position calculation and plot
+
+%%
 close all
 clear
 clc
@@ -7,8 +13,8 @@ clc
 fprintf('Script to plot the orientation');
 
 %% Code to Serial port selection
-COMPort1 = 'COM3';
-COMPort2 = 'COM4';
+COMPort1 = 'COM3';      % COM port to which the sensor 1 is connected
+COMPort2 = 'COM4';      % COM port to which the sensor 2 is connected
 
 %% Comunication parameters
 baudrate = 921600;          % rate at which information is transferred
@@ -16,6 +22,7 @@ lpSensor1 = lpms();         % function lpms API sensor 1 given by LPMS
 lpSensor2 = lpms();         % function lpms API sensor 2 given by LPMS
 
 %% Connect to sensor
+% Method to connect to the sensors
 disp('Connecting to sensors ...')
 if ( ~lpSensor1.connect(COMPort1, baudrate) )
     disp('Sensor not connected')
@@ -33,9 +40,10 @@ lpSensor1.setStreamingMode();
 lpSensor2.setStreamingMode();
 
 %% Physical data
+% data [leng_arm leng_elbow AnglesS1[3] AnglesS2[3]]
 a1 = 1;         % length arm
 a2 = 1;         % length elbow 
-data = [a1, a2, 0 , 0, 0, 0, 0, 0]; % data to plot
+data = [a1, a2, 0 , 0, 0, 0, 0, 0]; 
 
 %% Loop Plot
 disp('Plotting ...')
@@ -43,16 +51,16 @@ figure('doublebuffer','on', ...
        'CurrentCharacter','a', ...
        'WindowStyle','modal')
 set(gcf,'WindowStyle','normal');
-
+% create a figure and keep it until it closes 
 while double(get(gcf,'CurrentCharacter'))~=27
-    d1 = lpSensor1.getCurrentSensorData();
-    d2 = lpSensor2.getCurrentSensorData();
+    d1 = lpSensor1.getCurrentSensorData();      % get current sensor data
+    d2 = lpSensor2.getCurrentSensorData();      % get current sensor data
     if (~isempty(d1))
-        [X, Y, Z] = quat2angle(d1.quat, 'XYZ');
+        [X, Y, Z] = quat2angle(d1.quat, 'XYZ');     % convert quaternions to angles roll, pitch and yaw
         data(3) = X; data(4) = Y; data(5) = Z;
     end
     if (~isempty(d2))
-        [X, Y, Z] = quat2angle(d2.quat, 'XYZ');
+        [X, Y, Z] = quat2angle(d2.quat, 'XYZ');     % convert quaternions to angles roll, pitch and yaw
         data(6) = X; data(7) = Y; data(8) = Z;
     end
     funcPlot3DPosV4(data);
