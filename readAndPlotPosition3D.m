a65b6ca202686a53dbefb105ad45e181d1812dc3
Aldo Contreras <aldocontrego@gmail.com>
2018-01-24 11:06:42 +0100

    3D plotting
    Version 1.0

--- readAndPlotPosition3D.m
+++ readAndPlotPosition3D.m
@@ -35,34 +35,28 @@ lpSensor2.setStreamingMode();
 %% Physical data
 a1 = 1;         % length arm
 a2 = 1;         % length elbow 
-theta1 = 0;     % initial angle a1
-theta2 = 0;     % initial angle a2
-theta3 = 0;     % initial angle a3
-data = [a1, a2, theta1 , theta2, theta3];
+data = [a1, a2, 0 , 0, 0, 0, 0, 0]; % data to plot
 
 %% Loop Plot
 disp('Plotting ...')
 figure('doublebuffer','on', ...
        'CurrentCharacter','a', ...
        'WindowStyle','modal')
+set(gcf,'WindowStyle','normal');
 
 while double(get(gcf,'CurrentCharacter'))~=27
     d1 = lpSensor1.getCurrentSensorData();
     d2 = lpSensor2.getCurrentSensorData();
     if (~isempty(d1))
-        [yawS1, pitchS1, rollS1] = quat2angle(d1.quat);
-        data(3) = yawS1; % yaw or roll, depending the position of the sensor on the arm-frame
-        data(4) = rollS1; % yaw or roll, depending the position of the sensor on the arm-frame
+        [X, Y, Z] = quat2angle(d1.quat, 'XYZ');
+        data(3) = X; data(4) = Y; data(5) = Z;
     end
     if (~isempty(d2))
-        [yawS2, pitchS2, rollS2] = quat2angle(d2.quat);
-        data(5) = yawS2; % yaw or roll, depending the position of the sensor on the arm-frame
+        [X, Y, Z] = quat2angle(d2.quat, 'XYZ');
+        data(6) = X; data(7) = Y; data(8) = Z;
     end
-    %funcDrawSensor(data);
-    %funcPlot3DPos(data);
-    funcPlot3DPosV2(data)
+    funcPlot3DPosV4(data);
 end
-set(gcf,'WindowStyle','normal');
 
 %% Disconnecting
 disp('Done')
