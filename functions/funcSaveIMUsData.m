8ba7a6ab48a51324dc473dee7d68eac0ca9c6965
AldoContreGo <aldocontrego@gmail.com>
2018-09-06 12:06:37 +0200

    saving each IMU data
    saving each IMU data

--- functions/funcSaveIMUsData.m
+++ functions/funcSaveIMUsData.m
@@ -1,7 +1,19 @@
-function funcSaveIMUsData(num, data)
-    
-filename = "dataIMU"+ num + ".csv";
-fileID = fopen(filename,'a');
-fprintf(fileID,'%f %f\n',data');
-
+function funcSaveIMUsData(count, num, date, lpData)
+fileName = ['dataIMU',num2str(num),'_', date,'.txt'];
+if count <= 1
+    fileID = fopen(fileName, 'w+');
+    fprintf(fileID,'%12s %12s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s\r\n',...
+        'TimeStamp','Sample',...
+        'gyrX','gyrY','gyrZ','accX','accY','accZ','magX','magY','magZ',...
+        'quat1','quat2','quat2','quat2','eulerX','eulerY','eulerZ',...
+        'linAccX','linAccY','linAccZ');
+else
+    count = count - 1;
+    fileID = fopen(fileName, 'a');
+    fprintf(fileID,'%12.2f %12.0f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f\r\n',...
+        lpData.timestamp, count,...
+        lpData.gyr, lpData.acc, lpData.mag,...
+        lpData.quat, lpData.euler, lpData.linAcc);
+end
+fclose(fileID);
 end
